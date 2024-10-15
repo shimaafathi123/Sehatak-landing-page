b99ba0992c337a03bcf9cece17aefc53e94cf390
shimaaFathi <shimaafathi126@gmail.com>
2024-10-15 08:18:33 +0300

    update-header-mobile-view

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,28 @@ elements.forEach((element) => {
             el.style.color = '';   
             el.style.fontWeight = '';  
             el.style.opacity='';
+            el.style.backgroundColor="";
+
+            
    
         });
         this.style.color = 'rgb(50, 98, 174)';  
         this.style.fontWeight = 'bold';  
         this.style.opacity='1';
+        this.style.backgroundColor="white";
     });
+});
+
+function toggleMenu() {
+  const navLinks = document.getElementById('nav-links');
+  navLinks.classList.toggle('active');
+}
+
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  if (window.scrollY > 10) { 
+      header.style.boxShadow = '0 4px 16px rgba(0, 0, 0, 0.2)';
+  } else {
+      header.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+  }
 });
